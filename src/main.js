2ed352d31c42d938e4906750a9fbca9f3a5f40d4
Facu <romero.facundo010@gmail.com>
2026-08-29 09:20:32 -0300

    Agregué splash screen web con logo, nombre app y nombres de integrantes

--- src/main.js
+++ src/main.js
@@ -10,5 +10,17 @@ import { iniciarRouter } from './router.js';
 initialize();
 defineCustomElements(window);
 initSupabase();
-location.hash = '/mesas/alta'
+//location.hash = '/mesas/alta'
 iniciarRouter(document.querySelector('#app'));
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+    const splash = document.getElementById('web-splash');
+    if (splash) {
+        splash.style.opacity = '0'; // Inicia el desvanecimiento
+        setTimeout(() => {
+        splash.remove(); // La borra para que puedas usar la app
+        }, 500); // Dura lo mismo que la transición
+    }
+    }, 3500); // 2.5 segundos en pantalla
+});
